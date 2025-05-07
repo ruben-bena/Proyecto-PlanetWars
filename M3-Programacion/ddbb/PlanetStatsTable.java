a79c0d96a7c62f79a62cd89ee8146556dde317df
RubenBellidoNavarro <ruben.bellido.navarro@gmail.com>
2025-05-07 17:16:29 +0200

    Interface Table created. PlanetStatsTable started,  but realized that some work on the programming side is required beforehand

--- M3-Programacion/ddbb/PlanetStatsTable.java
+++ M3-Programacion/ddbb/PlanetStatsTable.java
@@ -1,5 +1,33 @@
 package ddbb;
 
-public class PlanetStatsTable {
+// This class (and the others alike) receives a Database object at creation (to receive the Connection attribute) and in each method receives the "Gamedata" object
+// TODO Need the "Gamedata" object to be defined before continuing this task
+public class PlanetStatsTable implements Table {
+    private Database db;
+    public static void main(String[] args) {
+        
+    }
 
+    public void insertRow() {
+        
+    }
+
+    @Override
+    public void getRow() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRow'");
+    }
+
+    @Override
+    public void deleteRow() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteRow'");
+    }
+
+    @Override
+    public void modifyRow() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'modifyRow'");
+    }
+    
 }
--- M3-Programacion/ddbb/Table.java
+++ M3-Programacion/ddbb/Table.java
@@ -1,5 +1,9 @@
 package ddbb;
 
+// Defines all methods shared between Table objects
 public interface Table {
-    
+    public void insertRow();
+    public void getRow();
+    public void deleteRow();
+    public void modifyRow();
 }
