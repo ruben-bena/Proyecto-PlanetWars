4f63b4498313ba12bd0618e2c2e1a11dd71772d0
RubenBellidoNavarro <ruben.bellido.navarro@gmail.com>
2025-05-14 20:40:59 +0200

    player can select between new game and load game, but only new game is functional for now

--- M3-Programacion/classes/Main.java
+++ M3-Programacion/classes/Main.java
@@ -21,27 +21,55 @@ public class Main{
         GlobalContext.database = new Database(url, username, pass);
 
         // TODO: Que al principio pregunte si quieres una nueva partida, o cargar una partida de la bbdd
-        // String[] opciones = {"Nueva Partida", "Cargar Partida"};
-        // int eleccion = JOptionPane.showOptionDialog(
-        //         null,
-        //         "What do you prefer?",
-        //         "Inicio del juego",
-        //         JOptionPane.DEFAULT_OPTION,
-        //         JOptionPane.QUESTION_MESSAGE,
-        //         null,
-        //         opciones,
-        //         opciones[0]
-        // );
+        String[] opciones = {"New Game", "Load Game"};
+        int gameChoice = JOptionPane.showOptionDialog(
+                null,
+                "What do you prefer?",
+                "Booting the game",
+                JOptionPane.DEFAULT_OPTION, // this is ignored because we have our own options to choose from
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opciones,
+                opciones[0] // this is the default option
+        );
 
+        // Create Planet
         Planet planet = new Planet(1, 1, 200000, 40000, 3000, 3000);
         planet.newLightHunter(4);
         planet.newHeavyHunter(2);
         planet.newIonCannon(3);
         planet.newArmoredShip(1);
 
-        // Create PlanetStatsTable object with planet info, and inserting it in ddbb
-        GlobalContext.planetStatsTable = new PlanetStatsTable(GlobalContext.database, planet);
-        GlobalContext.planetStatsTable.insertRow();
+        switch (gameChoice) {
+
+            // new game
+            case (0):
+                System.out.println("Has elegido 'New Game'");
+
+                // Create PlanetStatsTable object with planet info, and inserting it in ddbb
+                GlobalContext.planetStatsTable = new PlanetStatsTable(GlobalContext.database, planet);
+                GlobalContext.planetStatsTable.insertRow();
+
+                break;
+
+            // load game
+            case (1):
+                System.out.println("Has elegido 'Load Game'");
+
+                // TODO: Ventana que permita elegir planeta (se muestra id + nombre)
+
+                // TODO: Una vez elegido el planeta, creamos un objeto PlanetStatsTable que llame a la ddbb y al id del planeta
+
+                // TODO: Una vez cargada la bbdd, actualizamos el objeto Planet con sus valores
+
+                break;
+        }
+
+        // New Game
+        
+        // Load Game
+
+        
 
         // Things to add: The ability to "fix" damaged troops.
 
